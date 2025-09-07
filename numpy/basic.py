5d8683c1a7e03b491f84cbb77337218d5a092aa4
rohit-447 <rohitkumar826344@gmail.com>
2025-09-07 19:50:21 +0530

    statted learning numpy

---
+++ numpy/basic.py
@@ -0,0 +1,34 @@
+#print of array
+import numpy as np
+array= np.array([1,3])
+print(array)
+
+#properties
+#.ndim: find the dimension of array
+#.shape : find the shape of array
+
+#types of array
+#1d array (vector) : only 1 rows 
+array1d=np.array([1,2])
+print(f'Dimension of array is : {array1d.ndim}')
+print(f'Shape of array is : {array1d.shape}')
+
+
+#2d array (MATRIX): any no. of  rows  and cols
+array2d=np.array([[1,2],
+                  [3,4]])
+print(f'Dimension of array is : {array2d.ndim}')
+print(f'Shape of array is : {array2d.shape}')
+
+#3d array (Tensor) : 3 axis
+array3d=np.array([[[1,2]],
+                  [[2,3]]])
+
+print(f'Dimension of array is : {array3d.ndim}')
+print(f'Shape of array is : {array3d.shape}')
+
+#nd array: n dimension
+ndarray=np.zeros((3,2,3,2))
+print(f'nd Array: {ndarray}')
+print(f'Dimension of array is : {ndarray.ndim}')
+print(f'Shape of array is : {ndarray.shape}')
