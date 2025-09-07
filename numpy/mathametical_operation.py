bed214bc3411e369c5791973976bda1993c23aea
rohit-447 <rohitkumar826344@gmail.com>
2025-09-07 21:17:24 +0530

    learn : mathematical operations

---
+++ numpy/mathametical_operation.py
@@ -0,0 +1,17 @@
+import numpy as np
+#numpy helps to dynamically update the values of without any loop
+
+arr=np.array([[1,1],[2,2]])
+print(f"Original array: {arr}")
+
+#Addition
+print(f'Addition : {arr+2}')
+
+#Subtracion
+print(f'Subtraction : {arr-1}')
+
+#Multiplication
+print(f"Muliplication : {arr*10}")
+
+#Division
+print(f'Division : {arr/2}')
